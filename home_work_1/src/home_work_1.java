94c7c96f0305e5cc491fb834248f6d094ae20937
ArtsiomMolchan <81825066+ArtsiomMolchan@users.noreply.github.com>
2021-04-03 21:06:51 +0300

    Add files via upload

--- home_work_1/src/home_work_1.java
+++ home_work_1/src/home_work_1.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class home_work_1 {
+public class home_work_1 {
+    public static void main(String[] args) {
+
+        int a = 42;
+        int b = 15;
+        double c = 42.5;
+
+        //System.out.println(c &= c); //java: bad operand types for binary operator '&'
+
+        System.out.println(Integer.toBinaryString(a & b)); // binary 1010
+    System.out.println(Integer.toBinaryString(a &= b)); // binary 1010
+    System.out.println(Integer.toBinaryString(a | b)); // binary 1111
+    System.out.println(Integer.toBinaryString(a |= b)); // binary 1111
+    System.out.println(a ^ b); //0
+    System.out.println(a ^= b); //0
+    System.out.println(a >> b); //0
+    System.out.println(a >>= b); //0
+    System.out.println(a >>> b); //0
+    System.out.println(a << b); //0
+    System.out.println(a <<= b); //0
+    System.out.println(a >>>= b); //0
+
+
+        int x = -42;
+        int y = -15;
+
+        System.out.println(Integer.toBinaryString(x & y)); // binary 11111111111111111111111111010000
+        System.out.println(Integer.toBinaryString(x &= y)); // binary 11111111111111111111111111010000
+        System.out.println(Integer.toBinaryString(x | y)); //binary 11111111111111111111111111010001
+        System.out.println(Integer.toBinaryString(x |= y)); //binary 11111111111111111111111111010001
+        System.out.println(x ^ y); //0
+        System.out.println(x ^= y); //0
+        System.out.println(x >> y); //0
+        System.out.println(x >>= y); //0
+        System.out.println(x >>> y); //0
+        System.out.println(x << y); //0
+        System.out.println(x <<= y); //0
+        System.out.println(x >>>= y); //0
+    }
 }
--- home_work_1/src/home_work_1_2.java
+++ home_work_1/src/home_work_1_2.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class home_work_1_2 {
+public class home_work_1_2 {
+
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 2;
+        int c = 5 + b/ a; // 5 + 2/8 = 5.25 => 5
+        System.out.println(c);
+        int d = (5 + 2) / a; // (5 + 2) / 8 = 0,87 => 0
+        System.out.println(d);
+        int e = (5 + b++) / a; // (5 + 3) / a = 1 => 0
+        System.out.println(e);
+        int f = (5 + b++) / --a; // (5 + 3) / 7 = 1.14 =>1
+        System.out.println(f);
+        int g = (5 * 2 >> b++) / --a; // 1/1 = 1 => 0
+        System.out.println(g);
+        int h = (5 + 7 > 20 ? 68 : 22 * 2 >> b++) / --a; // 5/7 = 0.7 => 0
+        System.out.println(h);
+        //var i = (5 + 7 > 20 ? 68 >= 68 : 22 * 2 >> b++) / --a; can't resolve
+        //System.out.println(i);
+        boolean j = 6 - 2 > 3 && 12 * 12 <= 119; // true / false => false
+        System.out.println(j);
+        boolean k = true && false; // false => false
+        System.out.println(k);
+    }
 }
