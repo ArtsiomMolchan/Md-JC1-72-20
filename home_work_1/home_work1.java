523b7ad2273be552e2533d8819c10eef229174f9
Artsiom <papaz.live@mail.com>
2021-04-03 21:02:21 +0300

    part II

---
+++ home_work_1/home_work1.java
@@ -0,0 +1,22 @@
+public class HomeWork {
+    static int a = 42;
+    static int b = 15;
+
+    public static void main(String[] args) {
+        System.out.println(a ~ b);
+        System.out.println(a & b);
+        System.out.println(a &= b);
+        System.out.println(a | b);
+        System.out.println(a |= b);
+        System.out.println(a ^ b);
+        System.out.println(a ^= b);
+        System.out.println(a >> b);
+        System.out.println(a >>= b);
+        System.out.println(a >>> b);
+        System.out.println(a << b);
+        System.out.println(a <<= b);
+        System.out.println(a >>>= b);
+    }
+
+
+}
